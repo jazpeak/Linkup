6b7df0a08e7b05de55f5271ae304138ca7029f6f
jazpeak <jazl24283@iiitd.ac.in>
2026-07-19 23:06:26 +0530

    add backend docker and github actions workflow

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch(error){
         console.log("error connecting to MongoDB:", error.message);
